69cad14cc8c9dad198bdaee1beb9d701e2c8aadd
Fabio Crisci <piuccio@gmail.com>
2018-10-19 14:48:16 +0900

    go crazy with rollup output files

--- rollup.config.js
+++ rollup.config.js
@@ -5,25 +5,90 @@ import minify from 'rollup-plugin-babel-minify';
 import buble from 'rollup-plugin-buble';
 import pkg from './package.json';
 
-export default {
-  input: 'index.js',
-  plugins: [
-    flow(),
-    flowEntry(),
-    buble(),
-    filesize({ showMinifiedSize: false }),
-    minify({ comments: false, sourceMap: true }),
-  ],
-  external: Object.keys(pkg.peerDependencies),
-  output: [{
-    file: 'dist/es5/bundle.cjs.min.js',
-    format: 'cjs',
-  }, {
-    file: 'dist/es5/bundle.iife.min.js',
-    format: 'iife',
-    name: 'DurationUnitFormat',
-    globals: {
-      'intl-messageformat': 'IntlMessageFormat',
-    },
-  }],
-};
+export default [
+  // Non minified version ES5 version
+  {
+    input: 'index.js',
+    plugins: [
+      flow(),
+      flowEntry(),
+      buble(),
+    ],
+    external: Object.keys(pkg.peerDependencies),
+    output: [{
+      file: 'dist/es5/bundle.cjs.js',
+      format: 'cjs',
+    }, {
+      file: 'dist/es5/bundle.iife.js',
+      format: 'iife',
+      name: 'DurationUnitFormat',
+      globals: {
+        'intl-messageformat': 'IntlMessageFormat',
+      },
+    }],
+  },
+  // Minified ES5 verions
+  {
+    input: 'index.js',
+    plugins: [
+      flow(),
+      flowEntry(),
+      buble(),
+      minify({ comments: false, sourceMap: true }),
+    ],
+    external: Object.keys(pkg.peerDependencies),
+    output: [{
+      file: 'dist/es5/bundle.cjs.min.js',
+      format: 'cjs',
+    }, {
+      file: 'dist/es5/bundle.iife.min.js',
+      format: 'iife',
+      name: 'DurationUnitFormat',
+      globals: {
+        'intl-messageformat': 'IntlMessageFormat',
+      },
+    }],
+  },
+  // Non minified version ES6 version
+  {
+    input: 'index.js',
+    plugins: [
+      flow(),
+      flowEntry(),
+    ],
+    external: Object.keys(pkg.peerDependencies),
+    output: [{
+      file: 'dist/es6/bundle.cjs.js',
+      format: 'cjs',
+    }, {
+      file: 'dist/es6/bundle.iife.js',
+      format: 'iife',
+      name: 'DurationUnitFormat',
+      globals: {
+        'intl-messageformat': 'IntlMessageFormat',
+      },
+    }],
+  },
+  // Minified ES6 verions
+  {
+    input: 'index.js',
+    plugins: [
+      flow(),
+      flowEntry(),
+      filesize({ showMinifiedSize: false }),
+      minify({ comments: false, sourceMap: true }),
+    ],
+    external: Object.keys(pkg.peerDependencies),
+    output: [{
+      file: 'dist/es6/bundle.cjs.min.js',
+      format: 'cjs',
+    }, {
+      file: 'dist/es6/bundle.iife.min.js',
+      format: 'iife',
+      name: 'DurationUnitFormat',
+      globals: {
+        'intl-messageformat': 'IntlMessageFormat',
+      },
+    }],
+  },
+];
