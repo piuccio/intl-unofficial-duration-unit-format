3281b9a12cee7c5e3cc66f8296df2314b202bf23
Fabio Crisci <piuccio@gmail.com>
2019-08-09 13:10:07 +0900

    is this enough?

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,13 +1,1 @@
 require('intl-pluralrules');
-
-const {default: areIntlLocalesSupported} = require('intl-locales-supported');
-const localesMyAppSupports = [
-  'en',
-];
-
-// Determine if the built-in `Intl` has the locale data we need.
-if (!areIntlLocalesSupported(localesMyAppSupports)) {
-  // `Intl` exists, but it doesn't have the data we need, so load the
-  // polyfill and replace the constructors we need with the polyfill's.
-  require('@formatjs/intl-relativetimeformat/polyfill');
-}
