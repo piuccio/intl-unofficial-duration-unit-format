9e0b5d0d935a2bd89b1de950ebb66d2f99e5e0a4
Fabio Crisci <piuccio@gmail.com>
2020-04-11 17:17:11 +0900

    Use Intl.NumberFormat to generate CLDR styles (#12)

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,1 +1,11 @@
 require('intl-pluralrules');
+// NumberFormat polyfill needed on node 10
+require('@formatjs/intl-unified-numberformat/polyfill');
+if (typeof Intl.NumberFormat.__addLocaleData === 'function') {
+  Intl.NumberFormat.__addLocaleData(
+    require('@formatjs/intl-unified-numberformat/dist/locale-data/en.json'),
+  );
+  Intl.NumberFormat.__addLocaleData(
+    require('@formatjs/intl-unified-numberformat/dist/locale-data/ja.json'),
+  );
+}
---
+++ tests/units-durations.test.js
@@ -0,0 +1,67 @@
+import DurationUnitFormat from '../index';
+
+describe('formatToParts', () => {
+  it('formats to parts with custom format for units and durations', () => {
+    const parts = DurationUnitFormat.prototype.formatToParts.bind(new DurationUnitFormat('en', {
+      style: 'custom',
+      format: '{hours} + {minutes}',
+      formatDuration: '{unit}: {value}',
+      formatUnits: {
+        hour: 'H',
+        minute: 'M',
+      },
+    }));
+
+    expect(parts(0)).toEqual([
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '0' },
+    ]);
+    expect(parts(1)).toEqual([
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '0' },
+    ]);
+    expect(parts(30)).toEqual([
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '0' },
+    ]);
+    expect(parts(60)).toEqual([
+      { type: 'literal', value: ' + ' },
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '1' },
+    ]);
+    expect(parts(119)).toEqual([
+      { type: 'literal', value: ' + ' },
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '1' },
+    ]);
+    expect(parts(60 * 60 - 40)).toEqual([ // 40 seconds before 1h
+      { type: 'literal', value: ' + ' },
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '59' },
+    ]);
+    expect(parts(60 * 60 - 1)).toEqual([ // 1 second before 1h
+      { type: 'literal', value: ' + ' },
+      { type: 'unit', value: 'M' },
+      { type: 'literal', value: ': ' },
+      { type: 'minute', value: '59' },
+    ]);
+    expect(parts(10 * 60 * 60)).toEqual([
+      { type: 'unit', value: 'H' },
+      { type: 'literal', value: ': ' },
+      { type: 'hour', value: '10' },
+      { type: 'literal', value: ' + ' },
+    ]);
+    expect(parts(100 * 60 * 60)).toEqual([
+      { type: 'unit', value: 'H' },
+      { type: 'literal', value: ': ' },
+      { type: 'hour', value: '100' },
+      { type: 'literal', value: ' + ' },
+    ]);
+  });
+});
