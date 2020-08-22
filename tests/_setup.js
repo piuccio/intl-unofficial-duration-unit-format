055f6feb1cd1a71498a739fdeeca59381dddb2d1
Fabio Crisci <piuccio@gmail.com>
2020-08-22 20:51:50 +0900

    Update dependencies (#15)

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,11 +1,11 @@
 require('intl-pluralrules');
 // NumberFormat polyfill needed on node 10
-require('@formatjs/intl-unified-numberformat/polyfill');
+require('@formatjs/intl-numberformat/polyfill');
 if (typeof Intl.NumberFormat.__addLocaleData === 'function') {
   Intl.NumberFormat.__addLocaleData(
-    require('@formatjs/intl-unified-numberformat/dist/locale-data/en.json'),
+    require('@formatjs/intl-numberformat/dist/locale-data/en.json'),
   );
   Intl.NumberFormat.__addLocaleData(
-    require('@formatjs/intl-unified-numberformat/dist/locale-data/ja.json'),
+    require('@formatjs/intl-numberformat/dist/locale-data/ja.json'),
   );
 }
