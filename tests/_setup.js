d965bb9150a486263d312e8d80ab07b2d5f99abd
Brett Zamir <brettz9@yahoo.com>
2021-06-08 16:28:09 +0800

    - Breaking change: Require Node 12 (Node 10 end-of-lifed, use `flatMap`) - Refactoring: Use `flatMap` - Refactoring: ES6 class

--- tests/_setup.js
+++ tests/_setup.js
@@ -2,10 +2,6 @@ require('intl-pluralrules');
 // NumberFormat polyfill needed on node 10
 require('@formatjs/intl-numberformat/polyfill');
 if (typeof Intl.NumberFormat.__addLocaleData === 'function') {
-  Intl.NumberFormat.__addLocaleData(
-    require('@formatjs/intl-numberformat/dist/locale-data/en.json'),
-  );
-  Intl.NumberFormat.__addLocaleData(
-    require('@formatjs/intl-numberformat/dist/locale-data/ja.json'),
-  );
+  require('@formatjs/intl-numberformat/locale-data/en.js');
+  require('@formatjs/intl-numberformat/locale-data/ja.js');
 }
