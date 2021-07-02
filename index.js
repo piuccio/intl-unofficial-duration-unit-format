51d2261775a83c7fe81a4a3f93e5985c085f850d
Brett Zamir <brettz9@yahoo.com>
2021-07-02 13:27:59 +0800

    Refactor code using ES6 class
    Breaking change: Removes support for node 10

--- index.js
+++ index.js
@@ -1,21 +1,110 @@
 import IntlMessageFormat from 'intl-messageformat';
 import trim from './lib/trim';
 
-function DurationUnitFormat(locales, options = defaultOptions) {
-  this.locales = locales;
-  // TODO I'm ignoring the unit for now, value is always expressed in seconds
-  this.unit = 'second';
-  // .style determines how the placeholders are converted to plain text
-  this.style = options.style || DurationUnitFormat.styles.LONG;
-  // .isTimer determines some special behaviour, we want to keep the 0s
-  this.isTimer = this.style === DurationUnitFormat.styles.TIMER;
-  // .format used `seconds`, `minutes`, `hours`, ... as placeholders
-  this._format = options.format || (this.isTimer ? '{minutes}:{seconds}' : '{seconds}');
-  // How to format unit according to style
-  this.formatUnits = options.formatUnits || defaultOptions.formatUnits;
-  // .formatDuration determines whether we use a space or not
-  this.formatDuration = options.formatDuration || defaultOptions.formatDuration;
-  this.shouldRound = options.round === true;
+class DurationUnitFormat {
+  constructor (locales, options = defaultOptions) {
+    this.locales = locales;
+    // TODO I'm ignoring the unit for now, value is always expressed in seconds
+    this.unit = 'second';
+    // .style determines how the placeholders are converted to plain text
+    this.style = options.style || DurationUnitFormat.styles.LONG;
+    // .isTimer determines some special behaviour, we want to keep the 0s
+    this.isTimer = this.style === DurationUnitFormat.styles.TIMER;
+    // .format used `seconds`, `minutes`, `hours`, ... as placeholders
+    this._format = options.format || (this.isTimer ? '{minutes}:{seconds}' : '{seconds}');
+    // How to format unit according to style
+    this.formatUnits = options.formatUnits || defaultOptions.formatUnits;
+    // .formatDuration determines whether we use a space or not
+    this.formatDuration = options.formatDuration || defaultOptions.formatDuration;
+    this.shouldRound = options.round === true;
+  }
+
+  format (value) {
+    return this.formatToParts(value).map(({ value }) => value).join('');
+  }
+
+  formatToParts (value) {
+    // Extract all the parts that are actually used from the localised format
+    const parts = new IntlMessageFormat(this._format, this.locales).formatToParts({
+      second: { unit: DurationUnitFormat.units.SECOND },
+      seconds: { unit: DurationUnitFormat.units.SECOND },
+      minute: { unit: DurationUnitFormat.units.MINUTE },
+      minutes: { unit: DurationUnitFormat.units.MINUTE },
+      hour: { unit: DurationUnitFormat.units.HOUR },
+      hours: { unit: DurationUnitFormat.units.HOUR },
+      day: { unit: DurationUnitFormat.units.DAY },
+      days: { unit: DurationUnitFormat.units.DAY },
+    });
+    // Compute the value of each bucket depending on which parts are used
+    const buckets = splitSecondsInBuckets(value, this.unit, parts, this.shouldRound);
+    // Each part from the format message could potentially contain multiple parts
+    const result = parts.flatMap((token) => this._formatToken(token, buckets));
+    return this._trimOutput(result, parts);
+  }
+
+  _formatToken (token, buckets) {
+    const {value} = token;
+    if (value.unit) {
+      const number = buckets[value.unit];
+      return (number || this.isTimer) ? this._formatDurationToParts(value.unit, number) : [];
+    } else if (value) {
+      // If there is no .unit it's text, but it could be an empty string
+      return[{ type: 'literal', value }];
+    }
+    return [];
+  }
+
+  _formatDurationToParts (unit, number) {
+    if (this.isTimer) {
+      // With timer style, we only show the value
+      return [{ type: unit, value: this._formatValue(number) }];
+    } else if (isSpecialStyle(this.style)) {
+      return new Intl.NumberFormat(this.locales, {
+        style: 'unit',
+        unit: unit,
+        unitDisplay: this.style,
+      }).formatToParts(number).map((_) => ({
+        // NumberFormat uses 'integer' for types, but I prefer using the unit
+        // This is more similar to what happens in DateTimeFormat
+        type: _.type === 'integer' ? unit : _.type,
+        value: _.value,
+      }));
+    }
+    // This is now only needed for the custom formatting
+    return this.formatDuration.split(SPLIT_POINTS).map((text) => {
+      if (text === '{value}') {
+        return { type: unit, value: this._formatValue(number) };
+      }
+      if (text === '{unit}') {
+        const message = this.formatUnits[unit] || '{value}';
+        const formattedUnit = new IntlMessageFormat(message, this.locales).format({ value: number });
+        return { type: 'unit', value: formattedUnit };
+      }
+      if (text) {
+        return { type: 'literal', value: text };
+      }
+    }).filter(Boolean);
+  }
+
+  _formatValue (number) {
+    return this.isTimer ? number.toString().padStart(2, '0') : number.toString();
+  }
+
+  _trimOutput (result, parts) {
+    const trimmed = trim(result, this.isTimer);
+    if (!trimmed.find((_) => _.type !== 'literal')) {
+      // if everything cancels out and there are only literals,
+      // then return 0 on the lowest available unit
+      const minUnit = [
+        DurationUnitFormat.units.SECOND,
+        DurationUnitFormat.units.MINUTE,
+        DurationUnitFormat.units.HOUR,
+        DurationUnitFormat.units.DAY,
+      ].find((unit) => has(parts, unit));
+      return this._formatDurationToParts(minUnit, 0);
+    }
+    return trimmed;
+  }
 }
 
 DurationUnitFormat.units = {
@@ -32,93 +121,6 @@ DurationUnitFormat.styles = {
   LONG: 'long',
   SHORT: 'short',
   NARROW: 'narrow',
-};
-
-DurationUnitFormat.prototype.format = function (value) {
-  return this.formatToParts(value).map(({ value }) => value).join('');
-};
-
-DurationUnitFormat.prototype.formatToParts = function (value) {
-  // Extract all the parts that are actually used from the localised format
-  const parts = new IntlMessageFormat(this._format, this.locales).formatToParts({
-    second: { unit: DurationUnitFormat.units.SECOND },
-    seconds: { unit: DurationUnitFormat.units.SECOND },
-    minute: { unit: DurationUnitFormat.units.MINUTE },
-    minutes: { unit: DurationUnitFormat.units.MINUTE },
-    hour: { unit: DurationUnitFormat.units.HOUR },
-    hours: { unit: DurationUnitFormat.units.HOUR },
-    day: { unit: DurationUnitFormat.units.DAY },
-    days: { unit: DurationUnitFormat.units.DAY },
-  });
-  // Compute the value of each bucket depending on which parts are used
-  const buckets = splitSecondsInBuckets(value, this.unit, parts, this.shouldRound);
-  // Each part from the format message could potentially contain multiple parts
-  const result = parts.reduce((all, token) => all.concat(this._formatToken(token, buckets)), []);
-  return this._trimOutput(result, parts);
-};
-
-DurationUnitFormat.prototype._formatToken = function(token, buckets) {
-  const {value} = token;
-  if (value.unit) {
-    const number = buckets[value.unit];
-    return (number || this.isTimer) ? this._formatDurationToParts(value.unit, number) : [];
-  } else if (value) {
-    // If there is no .unit it's text, but it could be an empty string
-    return[{ type: 'literal', value }];
-  }
-  return [];
-}
-
-DurationUnitFormat.prototype._formatDurationToParts = function(unit, number) {
-  if (this.isTimer) {
-    // With timer style, we only show the value
-    return [{ type: unit, value: this._formatValue(number) }];
-  } else if (isSpecialStyle(this.style)) {
-    return new Intl.NumberFormat(this.locales, {
-      style: 'unit',
-      unit: unit,
-      unitDisplay: this.style,
-    }).formatToParts(number).map((_) => ({
-      // NumberFormat uses 'integer' for types, but I prefer using the unit
-      // This is more similar to what happens in DateTimeFormat
-      type: _.type === 'integer' ? unit : _.type,
-      value: _.value,
-    }));
-  }
-  // This is now only needed for the custom formatting
-  return this.formatDuration.split(SPLIT_POINTS).map((text) => {
-    if (text === '{value}') {
-      return { type: unit, value: this._formatValue(number) };
-    }
-    if (text === '{unit}') {
-      const message = this.formatUnits[unit] || '{value}';
-      const formattedUnit = new IntlMessageFormat(message, this.locales).format({ value: number });
-      return { type: 'unit', value: formattedUnit };
-    }
-    if (text) {
-      return { type: 'literal', value: text };
-    }
-  }).filter(Boolean);
-}
-
-DurationUnitFormat.prototype._formatValue = function (number) {
-  return this.isTimer ? number.toString().padStart(2, '0') : number.toString();
-};
-
-DurationUnitFormat.prototype._trimOutput = function (result, parts) {
-  const trimmed = trim(result, this.isTimer);
-  if (!trimmed.find((_) => _.type !== 'literal')) {
-    // if everything cancels out and there are only literals,
-    // then return 0 on the lowest available unit
-    const minUnit = [
-      DurationUnitFormat.units.SECOND,
-      DurationUnitFormat.units.MINUTE,
-      DurationUnitFormat.units.HOUR,
-      DurationUnitFormat.units.DAY,
-    ].find((unit) => has(parts, unit));
-    return this._formatDurationToParts(minUnit, 0);
-  }
-  return trimmed;
 };
 
 const defaultOptions = {
