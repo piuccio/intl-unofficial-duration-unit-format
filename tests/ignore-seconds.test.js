796cb86e23937a49ce182f29a0514cf43eb89fd5
Brett Zamir <brettz9@yahoo.com>
2022-06-03 14:02:35 +0800

    - feat: support `fields` (#24)

--- tests/ignore-seconds.test.js
+++ tests/ignore-seconds.test.js
@@ -96,4 +96,83 @@ describe('formatToParts', () => {
       { type: 'unit', value: 'hours' },
     ]);
   });
+
+  it('formats to parts with custom format restricted by `fields`', () => {
+    const parts = DurationUnitFormat.prototype.formatToParts.bind(new DurationUnitFormat('en', {
+      fields: ['minute', 'second'],
+      format: '{days} {hours} {minutes}',
+    }));
+
+    expect(parts(1)).toEqual([
+      { type: 'minute', value: '0' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(60)).toEqual([
+      { type: 'minute', value: '1' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minute' },
+    ]);
+    expect(parts(61)).toEqual([
+      { type: 'minute', value: '1' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minute' },
+    ]);
+    expect(parts(110)).toEqual([
+      { type: 'minute', value: '1' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minute' },
+    ]);
+    expect(parts(3600)).toEqual([
+      { type: 'minute', value: '60' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(3601)).toEqual([
+      { type: 'minute', value: '60' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(3660)).toEqual([
+      { type: 'minute', value: '61' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(3661)).toEqual([
+      { type: 'minute', value: '61' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(7179)).toEqual([
+      { type: 'minute', value: '119' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ])
+    expect(parts(7322)).toEqual([
+      { type: 'minute', value: '122' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(86400)).toEqual([
+      { type: 'minute', value: '1' },
+      { type: 'group', value: ',' },
+      { type: 'minute', value: '440' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(90000)).toEqual([
+      { type: 'minute', value: '1' },
+      { type: 'group', value: ',' },
+      { type: 'minute', value: '500' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+    expect(parts(180000)).toEqual([
+      { type: 'minute', value: '3' },
+      { type: 'group', value: ',' },
+      { type: 'minute', value: '000' },
+      { type: 'literal', value: ' ' },
+      { type: 'unit', value: 'minutes' },
+    ]);
+  });
 });
